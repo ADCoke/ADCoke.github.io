06b8bd14631620c9b1972abec7122faccb609205
ADCoke <moritz.sturmo@gmail.com>
2019-06-20 00:51:49 +0200

    a

--- js/main.js
+++ js/main.js
@@ -32,17 +32,17 @@ function setBG() {
 
   if (hour < 12) {
     document.body.style.backgroundImage = "url('./pics/wp_mstars.jpg')";
-    greeting.textContent = 'guten morgen mein geliebter hase';
+    greeting.textContent = '1';
 
   } else if (hour < 18) {
     document.body.style.backgroundImage = "url('./pics/wp_town.jpg')";
     document.body.style.color = "white";
-    greeting.textContent = 'mittag mit mir';
+    greeting.textContent = '2';
 
   } else {
     document.body.style.backgroundImage = "url('./pics/wp_citynite.jpg')";
     document.body.style.color = "white";
-    greeting.textContent = 'sleep tight';
+    greeting.textContent = '3';
 
   }
 
@@ -51,7 +51,7 @@ function setBG() {
 //Get name
 function getName() {
   if(localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
+    name.textContent = '[...]';
   } else {
     name.textContent = localStorage.getItem('name');
   }
